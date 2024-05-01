7daaa49bb38d8552e28aa5c673a9cdfdaf91197c
jericho31 <a@a.a>
2024-05-01 20:50:09 +0900

    aa titlecaseChar

--- src/aa.kt
+++ src/aa.kt
@@ -1,3 +1,16 @@
+//import java.util.Locale
+//
 //fun main() {
-//    println("Hello, world!")
+//    val charArr = charArrayOf('a', 'ω', '1', 'ŉ', 'A', '+', 'ß', 'ㄱ', 'ㅏ', '가')
+//    val strArr = arrayOf("this", "THIS", "tHis", "123abc", "hello, wORLd!")
+//    for (c in charArr) {
+//        println("${c.uppercase()}\t${c.titlecase()}\t${c.uppercaseChar()}\t${c.titlecaseChar()}")
+//    }
+//    println()
+//    for (s in strArr) {
+//        println(s.uppercase())
+//        println(s.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() })
+//        println(s.capitalize())
+//        println()
+//    }
 //}
